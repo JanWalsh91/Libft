9f51b0e40cd5d4f6a0ba4e8cd66c7100a0e4fdd1
Jan WALSH <jwalsh@e2r6p18.42.fr>
2017-01-03 11:08:02 +0100

    fixed straddcharfree

--- ft_straddcharfree.c
+++ ft_straddcharfree.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_straddchar.c                                    :+:      :+:    :+:   */
+/*   ft_straddcharfree.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwalsh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/01 11:17:40 by jwalsh            #+#    #+#             */
-/*   Updated: 2016/12/01 11:32:15 by jwalsh           ###   ########.fr       */
+/*   Created: 2017/01/03 11:07:36 by jwalsh            #+#    #+#             */
+/*   Updated: 2017/01/03 11:07:42 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 #include "libft.h"
 
-char	*ft_straddchar(char const *s, char c)
+char	*ft_straddcharfree(char const *s, char c)
 {
 	char	*newstr;
 	size_t	length;
