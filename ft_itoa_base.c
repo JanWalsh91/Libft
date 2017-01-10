f876897e8c559b90861e76e94e20bf31c5c41a95
Jan WALSH <jwalsh@e2r6p18.42.fr>
2017-01-10 15:42:44 +0100

    modifying ft_itoa_base

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 17:59:29 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/01/09 14:29:49 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/01/10 15:42:16 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ char	*ft_itoa_base(long value, int base)
 
 	if (base < 2)
 		return (0);
-	if (value == -2147483648)
-		return ("-2147483648");
+	if (value == - 9223372036854775807 - 1)
+		return (ft_strdup("-9223372036854775808"));
 	n = (value < 0 ) ? -value : value;
 	sign = (value < 0) ? -1 : 0;
 	i = (sign == -1) ? 2 : 1;
