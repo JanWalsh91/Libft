cc9c584c1d542e39154b35249dd498e3f1ad89a9
Jan WALSH <jwalsh@e3r9p5.42.fr>
2017-02-13 12:16:14 +0100

    fix atod

--- ft_to/ft_atod.c
+++ ft_to/ft_atod.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/13 11:17:18 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/02/13 11:26:32 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/02/13 12:15:05 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,27 @@ double  ft_atod(const char *str)
 {
     double  result;
     int     i;
-    double     divider;
+    double  divider;
+    double  sign;
 
     result = 0;
     if (!str)
         return (result);
     result = ft_atoi(str);
     i = 0;
+    while (str[i] == 9 || str[i] == 10 ||
+			str[i] == 11 || str[i] == 12 ||
+			str[i] == 13 || str[i] == 32)
+		i++;
     while (ft_isdigit(str[i]))
         ++i;
-    if (str[i] != ',')
+    if (str[i] != '.')
         return (result);
+    sign = (result < 0) ? -1 : 1;
     divider = 10;
     while (ft_isdigit(str[++i]))
     {
-        result += (str[i] - '0') / divider;
+        result += sign * (str[i] - '0') / divider;
         divider *= 10;
     }
     return (result);
