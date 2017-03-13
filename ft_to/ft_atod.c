07d7834140f48f09219f50e6fc22c902f74c5c47
Jan WALSH <jwalsh@e2r12p4.42.fr>
2017-03-13 12:44:07 +0100

    more fixes to ft_atod

--- ft_to/ft_atod.c
+++ ft_to/ft_atod.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/13 11:17:18 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/03/12 15:45:55 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/03/13 12:43:52 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,18 @@ double  ft_atod(const char *str)
     result = 0;
     if (!str)
         return (result);
+    sign = (ft_strchr(str, '-')) ? -1 : 1;
     result = ft_atoi(str);
     i = 0;
-    while (str[i] == 9 || str[i] == 10 ||
-			str[i] == 11 || str[i] == 12 ||
-			str[i] == 13 || str[i] == 32)
-		i++;
-    while (ft_isdigit(str[i]) || str[i] == '+' || str[i] == '-')
+    while (str[i] == 9 || str[i] == 10 || str[i] == 11 ||
+            str[i] == 12 || str[i] == 13 || str[i] == 32)
+		++i;
+    if (str[i] == '+' || str[i] == '-')
+        ++i;
+    while (ft_isdigit(str[i]))
         ++i;
     if (str[i] != '.')
         return (result);
-    sign = (result < 0) ? -1 : 1;
     divider = 10;
     while (ft_isdigit(str[++i]))
     {
